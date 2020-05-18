2119f15a3d0584c7b8a74472cca0cb46b19f0d72
Chencheng0302 <39509868+Chencheng0302@users.noreply.github.com>
2020-05-18 23:04:50 +0800

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,33 @@
+  /*var a,b,c;
+   a=$("#dh").offset().top;//元素相对于窗口的距离
+   b=$(window).scrollTop(); //监控窗口已滚动的距离;
+   c=$(document).height();//整个文档的高度
+   d=$(window).height();//浏览器窗口的高度
+   element  标签
+   cssname  动画
+   offset  相对于窗口的距离
+   */
+  //  function scrollnumber(element, offset) {
+  //     var a, b, c, height;
+  //     d = $(element).offset().top;//浏览器窗口的高度
+  //     a = eval(d + offset);//元素相对于窗口的距离
+  //     b = $(window).scrollTop();//窗口已滚动的距离
+  //     c = $(window).height();//整个文档的高度
+  //     if (b + c > a) {
+  //         $((element)).fadeIn();
+  //     }
+  // }
+  //
+  // function scrollfun() {
+  //     scrollnumber("#feature1", 1000);
+  //     // scrollnumber("#dh2", 'xz', 100);
+  //     // scrollnumber("#dh3", 'xz', 100);
+  //     // scrollnumber("#dh4", 'xz', 100);
+  // }
+
+  // $(document).ready(function (e) {
+  //     scrollfun();
+  //     $(window).scroll(function () {
+  //         scrollfun();
+  //     });
+  // });
